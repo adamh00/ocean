b2ce2086d25211514c7e20ec49fd63c62b9594cc
adamh00 <119237345+adamh00@users.noreply.github.com>
2022-12-05 21:13:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Xavki!");
-        System.out.println("it's me");
+        System.out.println("it's, me");
 
     }
 }
